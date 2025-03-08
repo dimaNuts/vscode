2024d83c03f4065ae67f19da36a6571374a410e8
nuts <nuts272@mail.ru>
2025-03-08 14:58:20 +0300

    create drawCats.js

---
+++ js/drawCats.js
@@ -0,0 +1,9 @@
+(function () {
+  let drawCats = function (howManyTimes) {
+    for (let i = 1; i <= howManyTimes; i++) {
+      console.log(i + "." + "=^.^=");
+    }
+  };
+
+  drawCats(5);
+})();
