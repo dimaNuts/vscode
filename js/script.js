440fcbb84711fd6eeb9c246b9fa365b8d33fe57f
nuts <nuts272@mail.ru>
2025-03-09 15:54:09 +0300

    exercise ch.6

--- js/script.js
+++ js/script.js
@@ -1,9 +1,56 @@
-let result = 3;
-// на одну итерацию больше, если приращение в консоли
-while (result < 10000) {
-  console.log((result *= 3));
+// beatiful animals
+let animals = ["Cat", "Fish", "Dog", "Bird"];
+
+let lenArray = animals.length;
+
+for (let i = 0; i < lenArray; i++) {
+  animals[i] += " - beautiful animals";
 }
 
-for (let result = 3; result < 10000; result *= 3) {
-  console.log(result);
+console.log(animals);
+
+// generator random string
+let alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+let randomString = "";
+let lenStr = alphabet.length;
+// +1 чтобы длина была больше нуля
+let randomLenStr = Math.floor(Math.random() * alphabet.length) + 1;
+while (randomString.length < randomLenStr) {
+  let randomIndexStr = Math.floor(Math.random() * lenStr);
+  let randomSymbol = alphabet[randomIndexStr];
+  randomString += randomSymbol;
 }
+
+console.log(randomString);
+
+// h4ck3r sp34k
+
+let input = "javascript is awesome!";
+let output = "";
+let lenInput = input.length;
+
+let changeLetter = {
+  a: "4",
+  e: "3",
+  i: "1",
+  o: "0",
+};
+
+let array = [];
+
+for (let i = 0; i < lenInput; i++) {
+  if (
+    input[i] === "a" ||
+    input[i] === "e" ||
+    input[i] === "i" ||
+    input[i] === "o"
+  ) {
+    array.push(changeLetter[input[i]]);
+  } else {
+    array.push(input[i]);
+  }
+}
+
+output = array.join("");
+
+console.log(output);
