8cf9b498c0d1b828601e68218189e006bf325b26
nuts <nuts272@mail.ru>
2025-03-13 14:48:03 +0300

    fix studyJQuer.js

--- js/studyJQuery.js
+++ js/studyJQuery.js
@@ -1,7 +1,11 @@
+// change heading
 let newHeadingText = prompt("Input new heading:", "");
 $("#main-heading").text(newHeadingText);
 
+// look version jQuery
 if (window.jQuery) {
   let vJq = jQuery.fn.jquery;
   console.log(vJq);
 }
+
+// поместить в конец страницы новый элемент p
