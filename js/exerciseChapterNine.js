ffc723c2e09fd6dd1a9ff63aa564c99ae575ecd5
nuts <nuts272@mail.ru>
2025-03-14 16:06:24 +0300

    add exerciseChapterNine.js

---
+++ js/exerciseChapterNine.js
@@ -0,0 +1,28 @@
+// Создаём массив с именами нескольких друзей
+let myFriends = ["Олег", "Нина", "Платон", "Ольга", "Саша", "Ирина"];
+
+// создаём для каждого имени по одному элементу p
+let lenarr = myFriends.length;
+
+for (let i = 0; i < lenarr; i++) {
+  $("body").append(`<p>${myFriends[i]}</p>`);
+}
+
+$("p").append(" the best!!!");
+
+$("h1").text("My friends!").hide(2000).slideDown(2000);
+
+// мигающий заголовок
+
+for (let i = 1; i <= 5; i++) {
+  $("h1")
+    .fadeOut(i * 1000)
+    .fadeIn(i * 1000);
+}
+
+// отложенная анимация
+
+$("h2").delay(5000).fadeOut(1000).fadeIn(2000);
+
+// fade to
+$("h2").fadeTo(10000, 0.1);
--- js/studyJQuery.js
+++ js/studyJQuery.js
@@ -16,3 +16,24 @@ for (let i = 0; i < 3; i++) {
   let hobby = prompt("Input you favorite hobby", "");
   $("body").append("<p>" + hobby + "</p>");
 }
+
+// fade out
+$("h1").fadeOut(3000);
+
+// fade in
+$("h1").text("Этот текст скоро исчезнет/ появится").fadeOut(3000).fadeIn(2000);
+
+// slide up / slide down
+
+$("h1")
+  .text("Этот текст скоро исчезнет")
+  .fadeOut(3000)
+  .slideUp(1000)
+  .slideDown(2000);
+
+for (let i = 0; i < 3; i++) {
+  $("h2").fadeOut(1000);
+  $("h2").fadeIn(1000);
+}
+
+$("h2").hide(1000).show(1000);
